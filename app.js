feae22b2e5f7a33deee587e28f7b42212c498c79
20136386 OSCAR VIRGEN CASILLAS <20136386@TELEMATICA.UCOL.MX>
2019-02-20 08:02:39 -0600

    function expression

--- app.js
+++ app.js
@@ -1,11 +1,23 @@
+
+//function statement
 function greet(){
-    console.log("Hello");
+    console.log("Hey");
 }
 
 greet();
 
+//function are first-class
 function logGreeting(funcion){
     funcion();
 }
 
 logGreeting(greet);
+
+//function expression
+let greetMe = function(){
+    console.log("Hey Oscar");
+}
+
+greetMe();
+
+logGreeting(greetMe);
