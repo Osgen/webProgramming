8c4e6a94076e2824d7faf39baf5f2a44fc44a3c5
20136386 OSCAR VIRGEN CASILLAS <20136386@TELEMATICA.UCOL.MX>
2019-03-11 08:15:06 -0600

    sixthClass->done

---
+++ app6.js
@@ -0,0 +1,50 @@
+
+// //Object literal
+// let Person ={
+//     firstName:"",
+//     lastName:"",
+//     greet:function(){
+//         return `${this.firstName} ${this.lastName}`;
+//     }
+// }
+
+// //this following objects are not the same as de object literal above
+// let oscar = Object.create(Person);
+// oscar.firstName="Oscar";
+// oscar.lastName="Virgen";
+
+// let daniel = Object.create(Person);
+
+// daniel.firstName="Daniel";
+// daniel.lastName="Deem";
+
+// console.log(oscar.greet());
+// console.log(daniel.greet());
+
+let EventEmitter = require("events");
+let utils =require("util");
+
+//create object with greeting text
+function Greetr() {
+    this.greeting = "Hello boe";
+}
+
+//adds eventEmitter to the heritance tree of the object created above
+utils.inherits(Greetr, EventEmitter);
+
+//greet is now a function of the object created
+Greetr.prototype.greet = function(who){
+    console.log(this.greeting+": "+who);
+    //beacause of the heritance, emit can by use by Greetr
+    this.emit("greet", who);
+}
+
+//instance a new object
+let greeter1 = new Greetr();
+
+//adds new listener to greet
+greeter1.on("greet", function(who){
+    console.log("someone greeted: "+who);
+});
+
+greeter1.greet("Me");
