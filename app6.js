e42a861f1fa3958bedf2e976bde456e237752119
20136386 OSCAR VIRGEN CASILLAS <20136386@TELEMATICA.UCOL.MX>
2019-03-11 08:26:28 -0600

    sixthClass->done for real

--- app6.js
+++ app6.js
@@ -1,50 +1,64 @@
 
-// //Object literal
-// let Person ={
-//     firstName:"",
-//     lastName:"",
-//     greet:function(){
-//         return `${this.firstName} ${this.lastName}`;
-//     }
+// // //Object literal
+// // let Person ={
+// //     firstName:"",
+// //     lastName:"",
+// //     greet:function(){
+// //         return `${this.firstName} ${this.lastName}`;
+// //     }
+// // }
+
+// // //this following objects are not the same as de object literal above
+// // let oscar = Object.create(Person);
+// // oscar.firstName="Oscar";
+// // oscar.lastName="Virgen";
+
+// // let daniel = Object.create(Person);
+
+// // daniel.firstName="Daniel";
+// // daniel.lastName="Deem";
+
+// // console.log(oscar.greet());
+// // console.log(daniel.greet());
+
+// let EventEmitter = require("events");
+// let utils =require("util");
+
+// //create object with greeting text
+// function Greetr() {
+//     this.greeting = "Hello boe";
 // }
 
-// //this following objects are not the same as de object literal above
-// let oscar = Object.create(Person);
-// oscar.firstName="Oscar";
-// oscar.lastName="Virgen";
+// //adds eventEmitter to the heritance tree of the object created above
+// utils.inherits(Greetr, EventEmitter);
 
-// let daniel = Object.create(Person);
+// //greet is now a function of the object created
+// Greetr.prototype.greet = function(who){
+//     console.log(this.greeting+": "+who);
+//     //beacause of the heritance, emit can by use by Greetr
+//     this.emit("greet", who);
+// }
 
-// daniel.firstName="Daniel";
-// daniel.lastName="Deem";
+// //instance a new object
+// let greeter1 = new Greetr();
 
-// console.log(oscar.greet());
-// console.log(daniel.greet());
+// //adds new listener to greet
+// greeter1.on("greet", function(who){
+//     console.log("someone greeted: "+who);
+// });
 
-let EventEmitter = require("events");
-let utils =require("util");
+// greeter1.greet("Me");
 
-//create object with greeting text
-function Greetr() {
-    this.greeting = "Hello boe";
+let obj ={
+    name:"daniel juan",
+    greet:function(){
+        console.log(`hello ${this.name}`);
+    }
 }
 
-//adds eventEmitter to the heritance tree of the object created above
-utils.inherits(Greetr, EventEmitter);
+obj.greet();
 
-//greet is now a function of the object created
-Greetr.prototype.greet = function(who){
-    console.log(this.greeting+": "+who);
-    //beacause of the heritance, emit can by use by Greetr
-    this.emit("greet", who);
-}
-
-//instance a new object
-let greeter1 = new Greetr();
-
-//adds new listener to greet
-greeter1.on("greet", function(who){
-    console.log("someone greeted: "+who);
-});
-
-greeter1.greet("Me");
+//call and aplly allow us to send params, which sustites the variable this
+//in the object function
+obj.greet.call({name:"Jane Juan"});
+obj.greet.apply({name:"Edgar Juan"});
