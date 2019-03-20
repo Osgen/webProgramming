998465161e37af2c5ce4ddc6769ba1c22903d6dd
20136386 OSCAR VIRGEN CASILLAS <20136386@TELEMATICA.UCOL.MX>
2019-03-20 08:10:46 -0600

    End for real

--- app7.js
+++ app7.js
@@ -1,17 +1,18 @@
 // let util = require("util");
 
+////OBJECT IN THE KNOWN WAY
 // function Person() {
 //     this.firstName ="Oscar";
 //     this.lastName ="Virgen";
 // }
-
+    ////CREATE FUNCTION GREET
 // Person.prototype.greet = function(){
 //     console.log(`Hey ${this.firstName} ${this.lastName}`);
 // }
 
 // function Policeman() {
 //     this.badgeNumber = "1234";
-//     Person.call(this);
+//     Person.call(this);//CALL FATHER
 // }
 
 // util.inherits(Policeman, Person);
@@ -22,18 +23,21 @@
 
 'use strict';
 
+
+//THIS IS CLASS STRUCTURE IN ES6
 class Person {
 
     constructor(first, last){
         this.firstName =first;
         this.lastName =last;
     }
-
+    //FUNCTION AS OBJECT ORIENTARED
     greet(){
         console.log(`Hey ${this.firstName} ${this.lastName}`);
     }
 }
 
+//INSTANCES AND FUNCTIONS
 let oscar = new Person("oscar", "virgen");
 oscar.greet();
 
