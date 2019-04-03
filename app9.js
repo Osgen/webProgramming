5d22855a54f6cbb9a1de97c53565428b8816998c
20136386 OSCAR VIRGEN CASILLAS <20136386@TELEMATICA.UCOL.MX>
2019-04-03 07:48:51 -0600

    End class ten

--- app9.js
+++
@@ -1,8 +0,0 @@
-let http = require("http");
-
-http.createServer(function (req, res) {
-    
-    res.writeHead(200, {'content-type': 'text/plain'});
-    res.end("Hellow world\n");
-
-}).listen(1337, 'localhost');
